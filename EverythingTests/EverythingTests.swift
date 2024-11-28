847c911c19d7e3b668da35c20f75e38551141782
raymondmutyaba <safer.04mats@icloud.com>
2024-11-27 18:01:16 -0800

    Initial Commit

---
+++ EverythingTests/EverythingTests.swift
@@ -0,0 +1,17 @@
+//
+//  EverythingTests.swift
+//  EverythingTests
+//
+//  Created by Raymond Mutyaba on 11/27/24.
+//
+
+import Testing
+@testable import Everything
+
+struct EverythingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ EverythingUITests/EverythingUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  EverythingUITestsLaunchTests.swift
+//  EverythingUITests
+//
+//  Created by Raymond Mutyaba on 11/27/24.
+//
+
+import XCTest
+
+final class EverythingUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
